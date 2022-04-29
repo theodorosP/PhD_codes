bd6933223f6556c4db913133d342a3e0da7fe949
Theodoros <83337690+theodorosP@users.noreply.github.com>
2022-04-29 13:05:01 -0400

    Update continue_target_potential.sh

--- bash_codes/continue_target_potential.sh
+++ bash_codes/continue_target_potential.sh
@@ -1,11 +1,14 @@
 #! /bin/bash
 
-b=`grep "CURRENT NELECT" OUTCAR | tail -1 | awk '{print $4}' `
-num=`ls -ltr|grep -c ^d`
-num=$(($num+1))
+a=`ls -ltr|grep -c ^d`
+num=$(echo "$a + 1"|bc)
 mkdir $num
 cp * $num
 rm -rf slurm*
 mv CONTCAR POSCAR
-sed -i "s/NELECT = 1131.7558665585047758/NELECT = $b/g" INCAR
+NELECT_OUTCAR=`grep "NELECT" OUTCAR|tail -1|awk '{print $4}'`
+echo $NELECT_OUTCAR
+NELECT_INCAR=`grep "NELECT" INCAR|tail -1|awk '{print $3}'`
+echo $NELECT_INCAR
+sed -i "s/NELECT = $NELECT_INCAR/NELECT = $NELECT_OUTCAR/g" INCAR
 sbatch job
