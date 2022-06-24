991939786b4f4b4adcf8de567ab31c376c4bfc4a
Theodoros <83337690+theodorosP@users.noreply.github.com>
2022-06-24 14:53:14 -0400

    Update change_INCAR.sh

--- bash_codes/change_INCAR.sh
+++ bash_codes/change_INCAR.sh
@@ -1,14 +1,15 @@
 #! /bin/bash
 
-for i in -2.5 -2.0 -1.0 -0.5 0.0
+for i in `seq -2.5 0.5 0.0`
+
 do
 
-cd /home/theodoros/PROJ_ElectroCat/theodoros/NH4_Bi111/NH4_Bi111_O_not_constrained/more_sites/right_left_4_symmetric/FindChg_U_$i/target_potential/more_accurate 
+cd /home/theodoros/PROJ_ElectroCat/theodoros/NH4_Bi111/NH4_Bi111_O_not_constrained/more_sites/NH4_top_symmetric/FindChg_$i/target_potential/more_accurate
 
 NELECT_INCAR=`grep "NELECT =" INCAR|awk '{print $3}'`
 echo NELECT_INCAR = $NELECT_INCAR
 
-NELECT_OUTCAR=`grep "NELECT" ../OUTCAR|head -1|awk '{print $3}'`
+NELECT_OUTCAR=`grep "NELECT" ../OUTCAR|tail -1|awk '{print $4}'`
 echo NELECT_OUTCAR = $NELECT_OUTCAR
 
 
@@ -26,9 +27,14 @@ echo TPOTVTARGET_UPDATED= $TPOTVTARGET_UPDATED
 
 sed -i "s/TPOTVTARGET = $TPOTVTARGET/TPOTVTARGET = $TPOTVTARGET_UPDATED/g" INCAR
 
-TPOTVTARGET=`grep "TPOTVTARGET" INCAR|awk '{print $3}'`
-echo TPOTVTARGET has changed to  $TPOTVTARGET
+#TPOTVTARGET=`grep "TPOTVTARGET" INCAR|awk '{print $3}'`
+#echo TPOTVTARGET has changed to  $TPOTVTARGET
 
 
+#cd /home/theodoros/PROJ_ElectroCat/theodoros/NH4_Bi111/NH4_Bi111_O_not_constrained/more_sites/right_left_4_symmetric/FindChg_U_$i/target_potential/
+#cp job more_accurate
+
+
+sed -i "s/#SBATCH -J NH4_top_U=0.0/#SBATCH -J NH4_top_U=$i/g" job
 echo -------------------
 done
